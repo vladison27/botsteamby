e98b57b388695a27ae3e6e44d8c6a83d43ade550
vladison27 <59660864+vladison27@users.noreply.github.com>
2020-01-09 07:52:20 +0400

    Add files via upload

--- botsteamby.py
+++ botsteamby.py
@@ -10,10 +10,22 @@ def main():
     password = driver.find_element_by_name("pass").send_keys("464fak10")
     vk_button = driver.find_element_by_id("install_allow")
     vk_button.click()
-    a = driver.find_element
+
 if __name__ == "__main__":
+    driver = webdriver.Chrome()
+    driver.get("https://steambuy.com/draw/")
+    button = driver.find_element_by_class_name("draw-item__btn_take")
+    button.click()
+    login = driver.find_element_by_name("email").send_keys("89524092018")
+    password = driver.find_element_by_name("pass").send_keys("464fak10")
+    vk_button = driver.find_element_by_id("install_allow")
+    vk_button.click()
+
     while True:
-        main()
-        print("Запись прошла успешно!")
-        print("Так, уже записался!")
-        time.sleep(14400)
+
+        try:
+            driver.refresh()
+            button.click()
+
+        except Exception:
+            time.sleep(3600)
